2dc62a2c41a6bc6b7529918776f958271e08b6d6
Leon <junzai12@icloud.com>
2025-03-01 12:30:44 -0800

    hi

---
+++ code/main.py
@@ -0,0 +1,40 @@
+import pygame
+
+# pygame setup
+pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+clock = pygame.time.Clock()
+running = True
+dt = 0
+
+player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+
+while running:
+    # pygame.QUIT event means the user clicked X to close your window
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            
+    bg = pygame.image.load("images/other/startscreen.png")
+    # fill the screen with a color to wipe away anything from last frame
+    screen.blit(bg, (0, -300))
+
+    pygame.draw.circle(screen, "red", player_pos, 40)
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w]:
+        player_pos.y -= 300 * dt
+    if keys[pygame.K_s]:
+        player_pos.y += 300 * dt
+    if keys[pygame.K_a]:
+        player_pos.x -= 300 * dt
+    if keys[pygame.K_d]:
+        player_pos.x += 300 * dt
+
+    # flip() the display to put your work on screen
+    pygame.display.flip()
+
+    # limits FPS to 60
+    dt = clock.tick(60) / 1000
+
+pygame.quit()
