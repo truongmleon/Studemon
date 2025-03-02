4a79a9fcc716f32c69a1608f38643fb5c8b5671d
Emi <Emily10005@hotmail.com>
2025-03-02 02:00:33 -0800

    added option buttons for volume and also a new page for a second type of button

---
+++ code/button2.py
@@ -0,0 +1,24 @@
+import pygame
+from settings import *
+
+class Button2:
+    def __init__(self, x, y, width, height, text, font, text_color, button_color):
+        self.rect = pygame.Rect(x, y, width, height)
+        self.text = text
+        self.font = font
+        self.text_color = text_color
+        self.button_color = button_color
+
+    def draw(self, surface):
+        # Draw button rectangle
+        pygame.draw.rect(surface, self.button_color, self.rect)
+        # Render text
+        text_surface = self.font.render(self.text, True, self.text_color)
+        text_rect = text_surface.get_rect(center=self.rect.center)
+        surface.blit(text_surface, text_rect)
+
+    def is_clicked(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if self.rect.collidepoint(event.pos):
+                return True
+        return False
--- code/pages/options.py
+++ code/pages/options.py
@@ -1,4 +1,5 @@
 import pygame
+from button2 import Button2
 from settings import *
 
 class Options:
@@ -6,13 +7,24 @@ class Options:
         pygame.init()
 
         self.window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-
         pygame.display.set_caption("Options")
+
         self.running = True
-
-        self.all_sprites = pygame.sprite.Group()
-
         self.clock = pygame.time.Clock()
+
+        # Font for buttons
+        self.font = pygame.font.Font(None, 36)
+
+        # Define buttons
+        self.pause_button = Button2(50, 100, 200, 50, "Pause Music", self.font, (255, 255, 255), (70, 120, 237))
+        self.unpause_button = Button2(50, 200, 200, 50, "Unpause Music", self.font, (255, 255, 255), (108, 209, 112))
+        self.stop_button = Button2(50, 300, 200, 50, "Stop Music", self.font, (255, 255, 255), (209, 108, 108))
+        self.volume_up_button = Button2(300, 100, 200, 50, "Volume +", self.font, (255, 255, 255), (208, 150, 219))
+        self.volume_down_button = Button2(300, 200, 200, 50, "Volume -", self.font, (255, 255, 255), (208, 150, 219))
+
+        # Current volume level
+        self.volume = 0.5  # Default volume (50%)
+        pygame.mixer.music.set_volume(self.volume)
 
     def run(self):
         while self.running:
@@ -22,13 +34,31 @@ class Options:
                 if event.type == pygame.QUIT:
                     self.running = False
 
+                # Handle button clicks
+                if self.pause_button.is_clicked(event):
+                    pygame.mixer.music.pause()
+                elif self.unpause_button.is_clicked(event):
+                    pygame.mixer.music.unpause()
+                elif self.stop_button.is_clicked(event):
+                    pygame.mixer.music.stop()
+                elif self.volume_up_button.is_clicked(event):
+                    self.volume = min(1.0, self.volume + 0.1)  # Increase volume (max 1.0)
+                    pygame.mixer.music.set_volume(self.volume)
+                elif self.volume_down_button.is_clicked(event):
+                    self.volume = max(0.0, self.volume - 0.1)  # Decrease volume (min 0.0)
+                    pygame.mixer.music.set_volume(self.volume)
+
+            # Draw the background
             bg = pygame.image.load("images/other/startscreen.png")
             self.window.blit(bg, (0, -300))
 
-            self.all_sprites.update(dt)
-
-            self.all_sprites.draw(self.window)
+            # Draw buttons
+            self.pause_button.draw(self.window)
+            self.unpause_button.draw(self.window)
+            self.stop_button.draw(self.window)
+            self.volume_up_button.draw(self.window)
+            self.volume_down_button.draw(self.window)
 
             pygame.display.update()
-            
+
         pygame.quit()
