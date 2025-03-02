c5b9015a6a6eac4f64e7f2bd8876b980ae68d419
corrze <107592289+corrze@users.noreply.github.com>
2025-03-01 20:57:00 -0800

    support

--- code/support.py
+++ code/support.py
@@ -1,4 +1,6 @@
 from settings import * 
+import pygame
+import os
 
 """
 Loads all images from a specified folder and returns a dictionary of surfaces.
@@ -15,16 +17,16 @@ def folder_importer(*path):
     surfs = {}
     
     # walk through the folder and get all the files
-    for folder_path, _, files in walk(join(*path)):
+    for folder_path, _, file_name in walk(join(*path)):
         # load all the images
-        for file_name in files:
-            full_path = join(folder_path, files)
+        for file_name in file_name:
+            full_path = join(folder_path, file_name)
             surfs[file_name.split('.')[0]] = pygame.image.load(full_path).convert_alpha()
     return surfs
 
 def audio_importer(*path):
     sounds = {}
-    for folder_path, _, files in walk(join(*path)):
-        for files in files:
-            sounds[files.split('.')[0]] = pygame.mixer.Sound(join(folder_path, files))
+    for folder_path, _, file_name in walk(join(*path)):
+        for file_name in file_name:
+            sounds[file_name.split('.')[0]] = pygame.mixer.Sound(join(folder_path, file_name))
     return sounds
